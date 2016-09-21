e723291846d1921f0996cf3039559c072236bb2d
andysungj <andysungj@126.com>
2016-09-21 23:42:44 +0800

    comments

--- app/src/main/java/com/aladdin/apps/questionbank/util/Constants.java
+++ app/src/main/java/com/aladdin/apps/questionbank/util/Constants.java
@@ -4,8 +4,8 @@ package com.aladdin.apps.questionbank.util;
  * Created by AndySun on 2016/9/19.
  */
 public class Constants {
-    //public static final String restfulEndpoints = "http://192.168.2.104:8081/AladdinSpringFour/";
-    public static final String restfulEndpoints = "http://10.10.8.102:8081/AladdinSpringFour/";
+    public static final String restfulEndpoints = "http://192.168.2.104:8081/AladdinSpringFour/";
+    //public static final String restfulEndpoints = "http://10.10.8.102:8081/AladdinSpringFour/";
     public static final String  getCustomersById= "questionBanks";
     public static final String  getLoginByAccountNumberUrl= "users/login/";
 }
